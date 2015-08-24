45f085c4b8a9ef6638c4e1da0bdfbcf9f8494615
Eugeniy Mikhailov <evgmik@gmail.com>
2015-08-23 23:40:20 -0400

    initial commit based on IWillFireNoBullet bot

---
+++ eem/EvBotNG.java
@@ -0,0 +1,17 @@
+// -*- java -*-
+
+package eem;
+
+import eem.frame.core.*;
+import robocode.*;
+
+/**
+ * EvBotNG - EvBot new generation robot by Eugeniy E. Mikhailov
+ */
+public class EvBotNG extends CoreBot {
+
+	public EvBotNG() {
+		super();
+	}
+};
+
